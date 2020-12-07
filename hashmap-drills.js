4fdc2643c467d02c6c64b501720aafbf46dc9df8
Courtney Carson <cmacarson@gmail.com>
2020-12-07 14:27:58 -0800

    question 5

--- hashmap-drills.js
+++ hashmap-drills.js
@@ -1,12 +1,17 @@
-const Hashmap = require('./hashmap');
 const HashMap = require('./hashmap');
 
+
+//1. Print your hash map and notice the length and items that are hashed in your hash map. Have you hashed all the items you were asked to?
+// Retrieve the value that is hashed in the key "Maiar" and Hobbit.
+// What are the values of Maiar and Hobbit that you have? Is there a 
+//discrepancy ? Explain your answer. What is the capacity of your hash
+//table after you have hashed all the above items ? Explain your answer.
 function main() {
 
-  const lotr = new Hashmap();
+  const lotr = new HashMap();
 
   HashMap.MAX_LOAD_RATIO = 0.5;
-  Hashmap.SIZE_RATIO = 3;
+  HashMap.SIZE_RATIO = 3;
 
   lotr.set('Hobbit', 'Bilbo');
   lotr.set('Hobbit', 'Frodo');
@@ -30,24 +35,54 @@ function main() {
 
 main();
 
-
-const WhatDoesThisDo = function(){
+//2.WhatDoesThisDo - What is the output of the following code? 
+//explain your answer.
+const WhatDoesThisDo = function () {
   let str1 = 'Hello World.';
   let str2 = 'Hello World.';
   let map1 = new HashMap();
-  map1.set(str1,10);
-  map1.set(str2,20);
+  map1.set(str1, 10);
+  map1.set(str2, 20);
   let map2 = new HashMap();
   let str3 = str1;
   let str4 = str2;
-  map2.set(str3,20);
-  map2.set(str4,10);
+  map2.set(str3, 20);
+  map2.set(str4, 10);
 
-  console.log(map1.get(str1));
-  console.log(map2.get(str3));
+  // console.log(map1.get(str1));
+  // console.log(map2.get(str3));
+};
+WhatDoesThisDo();
+
+// this will return only the value and not the key -- returns 20, 10
+//because they key is the same
+
+//4. Remove Duplicates -- Implement a function to delete all duplicated characters
+//in a string and keep only the first occurrence of each character. For example, if 
+//the input is string “google”, the result after deletion is “gole”.Test your program 
+//with a sentence as well such as "google all that you think can think of".
+
+function removeDuplicates(string) {
+
+
+  const answer = new HashMap(); 
+
+  let newString = ''; 
+
+  for (let char of string) {
+    if (!answer.get(char)) {
+      newString += char; 
+      answer.set(char, true); 
+    }
+  }
+  console.log(newString)
+
+  return newString; 
 }
-WhatDoesThisDo()
+removeDuplicates('google') 
 
-// this will return only the value and not the key -- returns 20, 10 
-//because they key is the same 
+
+// think of hashmap like table drawings one column with a label 
+// set to hashmap and set seen to true then if already in there 
+// if already there skip it 
 
